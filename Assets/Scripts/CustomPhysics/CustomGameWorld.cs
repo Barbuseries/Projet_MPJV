f5c63b2d79fa61ee1e61e0f127fa49463ff64434
Ronpiche <vgspm@hotmail.fr>
2017-12-11 10:00:57 -0500

    add custom collider for sphere

---
+++ Assets/Scripts/CustomPhysics/CustomGameWorld.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGameWorld : MonoBehaviour {
+
+	private CustomCollider[] _colliderList; // should be something more avanced than a simple list
+
+
+	public void Start(){
+		_colliderList = gameObject.GetComponentsInChildren<CustomCollider> ();
+		int id = 0;
+		foreach (CustomCollider currentCollider in _colliderList) {
+			//set an id for the collider
+			//for now only usefull to not make the object collide with itself
+			currentCollider.setId (id);
+			id += 1;
+		}
+	}
+
+	public void Update(){
+
+	}
+
+	public CustomCollider[] getColliderList(){
+		return _colliderList;
+	}
+
+
+}
