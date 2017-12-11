2b8390dc9ab5c0ea392f8593e2e6f968dfb00651
Alv0iD <dakoukou@hotmail.fr>
2017-12-10 23:16:59 -0500

    Sacré rebondissement dans le projet!

---
+++ Assets/Scripts/CustomPhysics/CustomSpringJoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("CustomPhysics/Custom Joint")]
+[RequireComponent(typeof(CustomRigidBody))]
+public class CustomSpringJoint : MonoBehaviour {
+
+    public GameObject connectedBody;
+    public GameObject self;
+   
+    public float spring;
+    //public float damper;
+    //public float minDistance;
+    //public float maxDistance;
+    // public float tolerance;
+
+    private CustomTransform selfTransform;
+    private CustomTransform connectTransform;
+    private CustomRigidBody connectRigid;
+
+    private Vector3 reposPos;
+
+    private void Start()
+    {
+        selfTransform = self.GetComponent<CustomTransform>();
+        connectTransform = connectedBody.GetComponent<CustomTransform>();
+        connectRigid = connectedBody.GetComponent<CustomRigidBody>();
+
+        reposPos = connectTransform.position;
+    }
+
+    void FixedUpdate () {
+
+        Vector3 axis = reposPos - connectTransform.position;
+        Vector3 springForce = spring * axis;
+        connectRigid.AddForce(springForce);
+
+    }
+}
