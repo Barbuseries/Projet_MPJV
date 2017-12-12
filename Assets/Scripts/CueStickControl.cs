18e6382e43c194ab542e57b3108d24e577461e59
Barbuseries <francois.edorh@isen.yncrea.fr>
2017-12-12 20:17:04 +0100

    Better than nothing

---
+++ Assets/Scripts/CueStickControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueStickControl : MonoBehaviour {
+	public CustomRigidBody cueStick;
+	public float maxDistance = 10f;
+
+	private CustomTransform _cueStickTransform;
+
+	private CustomSpringJoint _spring;
+	private CustomTransform _connectedTransform;
+	
+    // Use this for initialization
+    void Start () {
+        _cueStickTransform = cueStick.GetComponent<CustomTransform>();
+		_spring = cueStick.GetComponent<CustomSpringJoint>();
+		_connectedTransform = _spring.connectedBody.GetComponent<CustomTransform>();
+		
+		_spring.enabled = false;
+		cueStick.useGravity = false;
+    }
+
+	void FixedUpdate() {
+		bool leftButton = Input.GetMouseButton(0);
+		
+		if (leftButton) {
+			_spring.enabled = false;
+			
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = Camera.main.transform.position.y;
+		
+			Vector3 dragPos = Camera.main.ScreenToWorldPoint(mousePos);
+			Vector3 deltaPos = (dragPos - _connectedTransform.position);
+			
+			if (deltaPos.magnitude > maxDistance) {
+				dragPos = _connectedTransform.position + deltaPos.normalized * maxDistance;
+			}
+			
+			_cueStickTransform.position = dragPos;
+			cueStick.velocity = Vector3.zero;
+		}
+		else {
+			_spring.enabled = true;
+		}
+	}
+}
